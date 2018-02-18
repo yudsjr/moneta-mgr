96d6a123d3867562252bdc230d3592a1739578ad
Udel Dela Cruz Jr <udeljr.delacruz+heroku@gmail.com>
2018-02-19 04:58:02 +0800

    another set of changes

--- src/components/Transaction/SpeedDial.vue
+++ src/components/Transaction/SpeedDial.vue
@@ -1,13 +1,16 @@
 <template>
   <v-container fluid grid-list-xs>
-    <v-layout row justify-space-center pb-3 pt-5>
+    <!-- Header -->
+    <v-layout row pb-3 pt-5>
       <v-flex xs10 offset-xs1
         sm6 offset-sm3 left>
         <span class="headline">New Transaction</span>
       </v-flex>
     </v-layout>
+
+    <!--Date -->
     <v-layout row justify-space-center>
-      <v-flex xs6 offset-xs1 sm4 offset-sm1 left>
+      <v-flex xs10 offset-xs1 sm6 offset-sm3 left>
         <v-menu
           lazy
           :close-on-content-click="false"
@@ -38,31 +41,25 @@
           </v-date-picker>
         </v-menu>
       </v-flex>
-      <v-flex xs2 sm3 center>
-        <v-btn block round color="primary" large><</v-btn>
-      </v-flex>
-      <v-flex xs2 sm3 center>
-        <v-btn block round color="primary" large>></v-btn>
-      </v-flex>
     </v-layout>
 
     <!-- First row -->
     <v-layout align-center>
-      <v-flex xs5 offset-xs1 text-xs-center>
+      <v-flex xs5 offset-xs1 sm3 offset-sm3 text-xs-center>
         <div>
-          <v-btn block color="primary" dark style="height: 230%; padding: 32px" @click="showSpeedDialog = true">
+          <v-btn block color="primary" dark style="height: 230%; padding: 25px" @click="showSpeedDialog = true">
             <div>
-              <span class="headline">{{option1.category}}</span><br />
+              <span class="subheading">{{option1.category}}</span><br />
               <small>{{option1.account}}</small>
             </div>
           </v-btn>
         </div>
       </v-flex>
-      <v-flex xs5 text-xs-center>
+      <v-flex xs5 sm3 text-xs-center>
         <div>
-          <v-btn block color="primary" dark style="height: 230%; padding: 32px" @click="showSpeedDialog = true">
+          <v-btn block color="primary" dark style="height: 230%; padding: 25px" @click="showSpeedDialog = true">
             <div>
-              <span class="headline">{{option2.category}}</span><br />
+              <span class="subheading">{{option2.category}}</span><br />
               <small>{{option2.account}}</small>
             </div>
           </v-btn>
@@ -72,21 +69,21 @@
 
     <!-- Second row -->
     <v-layout align-center>
-      <v-flex xs5 offset-xs1 text-xs-center>
+      <v-flex xs5 offset-xs1 sm3 offset-sm3 text-xs-center>
         <div>
-          <v-btn block color="primary" dark style="height: 230%; padding: 32px" @click="showSpeedDialog = true">
+          <v-btn block color="primary" dark style="height: 230%; padding: 25px" @click="showSpeedDialog = true">
             <div>
-              <span class="headline">{{option3.category}}</span><br />
+              <span class="subheading">{{option3.category}}</span><br />
               <small>{{option3.account}}</small>
             </div>
           </v-btn>
         </div>
       </v-flex>
-      <v-flex xs5 text-xs-center>
+      <v-flex xs5 sm3 text-xs-center>
         <div>
-          <v-btn block color="primary" dark style="height: 230%; padding: 32px" @click="showSpeedDialog = true">
+          <v-btn block color="primary" dark style="height: 230%; padding: 25px" @click="showSpeedDialog = true">
             <div>
-              <span class="headline">{{option4.category}}</span><br />
+              <span class="subheading">{{option4.category}}</span><br />
               <small>{{option4.account}}</small>
             </div>
           </v-btn>
@@ -96,21 +93,21 @@
 
     <!-- Third row -->
     <v-layout align-center>
-      <v-flex xs5 offset-xs1 text-xs-center>
+      <v-flex xs5 offset-xs1 sm3 offset-sm3 text-xs-center>
         <div>
-          <v-btn block color="primary" dark style="height: 230%; padding: 32px" @click="showSpeedDialog = true">
+          <v-btn block color="primary" dark style="height: 230%; padding: 25px" @click="showSpeedDialog = true">
             <div>
-              <span class="headline">{{option5.category}}</span><br />
+              <span class="subheading">{{option5.category}}</span><br />
               <small>{{option5.account}}</small>
             </div>
           </v-btn>
         </div>
       </v-flex>
-      <v-flex xs5 text-xs-center>
+      <v-flex xs5 sm3 text-xs-center>
         <div>
-          <v-btn block color="primary" dark style="height: 230%; padding: 32px" @click="showSpeedDialog = true">
+          <v-btn block color="primary" dark style="height: 230%; padding: 25px" @click="showSpeedDialog = true">
             <div>
-              <span class="headline">{{option6.category}}</span><br />
+              <span class="subheading">{{option6.category}}</span><br />
               <small>{{option6.account}}</small>
             </div>
           </v-btn>
@@ -118,29 +115,34 @@
       </v-flex>
     </v-layout>
 
+    <!-- Manual Transaction -->
     <v-layout row justify-space-center class="pb-5">
-      <v-flex xs8 offset-xs2>
-        <v-btn block color="primary" dark style="height: 230%; width: 100%">New Transaction</v-btn>
+      <v-flex xs8 offset-xs2 sm4 offset-sm4>
+        <v-btn block color="primary" dark style="height: 150%; width: 100%">New Transaction</v-btn>
       </v-flex>
     </v-layout>
 
+    <!-- Dialog Window -->
     <v-dialog v-model="showSpeedDialog" persistent max-width="500px">
       <v-card>
-    	<v-card-title>
-    	  <span class="headline">User Profile</span>
-    	</v-card-title>
-    	<v-card-text>
-    	  <v-container grid-list-md>
-    		<v-layout wrap>
-    		  <large>content</large>
-    		</v-layout>
-    	  </v-container>
-    	</v-card-text>
-    	<v-card-actions>
-    	  <v-spacer></v-spacer>
-    	  <v-btn color="blue darken-1" flat @click.native="showSpeedDialog = false">Close</v-btn>
-    	  <v-btn color="blue darken-1" flat @click.native="showSpeedDialog = false">Save</v-btn>
-    	</v-card-actions>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-text-field
+                label="Amount"
+                prefix="P"
+              ></v-text-field>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="showSpeedDialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="showSpeedDialog = false">Save</v-btn>
+        </v-card-actions>
       </v-card>
     </v-dialog>
   </v-container>
