36fead791c56ff5b89af68b97f3fbcc2a2dbadc5
Udel Delacruz <Udel.Delacruz@MicroPact.com>
2018-03-30 22:00:01 +0800

    fullscreen overlay

--- src/home/Home.vue
+++ src/home/Home.vue
@@ -12,84 +12,196 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-      <v-toolbar color="indigo" dark fixed app>
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <v-toolbar-title>Application</v-toolbar-title>
-      </v-toolbar>
-        <v-content>
-          <v-container fluid>
-            <v-layout
-              justify-center
-              align-center
-            >
-              <v-flex xs12 sm6>
-                <v-list two-line mt-5>
-                  <template v-for="(item, index) in items">
-                    <v-list-tile avatar ripple :key="index" @click="">
-                      <v-list-tile-content>
-                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-                        <v-list-tile-sub-title class="text--primary">{{ item.headline }}</v-list-tile-sub-title>
-                        <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
-                      </v-list-tile-content>
-                      <v-list-tile-action>
-                        <v-list-tile-action-text>{{ item.action }}</v-list-tile-action-text>
-                        <v-icon color="grey lighten-1">star_border</v-icon>
-                      </v-list-tile-action>
-                    </v-list-tile>
-                    <v-divider v-if="index + 1 < items.length" :key="`divider-${index}`"></v-divider>
-                  </template>
-                </v-list>
-              </v-flex>
-            </v-layout>
 
-          </v-container>
-          <v-speed-dial
-            v-model="fab"
-            direction="top"
-            transition="slide-y-reverse-transition"
-            style="position: fixed; bottom: 50px; right: 16px;">
-            <v-btn
-              slot="activator"
-              color="blue darken-2"
-              dark
-              fab
-              hover
-              v-model="fab"
-            >
-              <v-icon>account_circle</v-icon>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+
+      <v-container fluid>
+        <v-layout justify-center align-center>
+          <v-flex xs12 sm6>
+            <v-list two-line mt-5>
+              <template v-for="(item, index) in itemscard">
+                <v-list-tile avatar ripple :key="index" @click="">
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    <v-list-tile-sub-title class="text--primary">{{ item.headline }}</v-list-tile-sub-title>
+                    <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                  <v-list-tile-action>
+                    <v-list-tile-action-text>{{ item.action }}</v-list-tile-action-text>
+                    <v-icon color="grey lighten-1">star_border</v-icon>
+                  </v-list-tile-action>
+                </v-list-tile>
+                <v-divider v-if="index + 1 < itemscard.length" :key="`divider-${index}`"></v-divider>
+              </template>
+            </v-list>
+          </v-flex>
+        </v-layout>
+      </v-container>
+
+      <v-btn color="primary" dark @click.stop="dialog = true">Open Dialog 1</v-btn>
+
+      <v-dialog
+        v-model="dialog"
+        fullscreen
+        transition="dialog-bottom-transition"
+        :overlay="false"
+        scrollable>
+        <v-card tile>
+          <v-toolbar card dark color="primary">
+            <v-btn icon @click.native="dialog = false" dark>
               <v-icon>close</v-icon>
             </v-btn>
-            <v-btn
-              fab
-              dark
-              small
-              color="green"
-            >
-              <v-icon>edit</v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              dark
-              small
-              color="indigo"
-            >
-              <v-icon>add</v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              dark
-              small
-              color="red"
-            >
-              <v-icon>delete</v-icon>
-            </v-btn>
-          </v-speed-dial>
-        </v-content>
+            <v-toolbar-title>Settings</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+              <v-btn dark flat @click.native="dialog = false">Save</v-btn>
+            </v-toolbar-items>
+            <v-menu bottom right offset-y>
+              <v-btn slot="activator" dark icon>
+                <v-icon>more_vert</v-icon>
+              </v-btn>
+              <v-list>
+                <v-list-tile v-for="(item, i) in items" :key="i" @click="">
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+            </v-menu>
+          </v-toolbar>
+          <v-card-text>
+            <v-btn color="primary" dark @click.stop="dialog2 = !dialog2">Open Dialog 2</v-btn>
+            <v-tooltip right>
+              <v-btn slot="activator">Tool Tip Activator</v-btn>
+              Tool Tip
+            </v-tooltip>
+            <v-list three-line subheader>
+              <v-subheader>User Controls</v-subheader>
+              <v-list-tile avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>Content filtering</v-list-tile-title>
+                  <v-list-tile-sub-title>Set the content filtering level to restrict apps that can be downloaded</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile avatar>
+                <v-list-tile-content>
+                  <v-list-tile-title>Password</v-list-tile-title>
+                  <v-list-tile-sub-title>Require password for purchase or use password to restrict purchase</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+            <v-divider></v-divider>
+            <v-list three-line subheader>
+              <v-subheader>General</v-subheader>
+              <v-list-tile avatar>
+                <v-list-tile-action>
+                  <v-checkbox v-model="notifications"></v-checkbox>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>Notifications</v-list-tile-title>
+                  <v-list-tile-sub-title>Notify me about updates to apps or games that I downloaded</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile avatar>
+                <v-list-tile-action>
+                  <v-checkbox v-model="sound"></v-checkbox>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>Sound</v-list-tile-title>
+                  <v-list-tile-sub-title>Auto-update apps at any time. Data charges may apply</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-list-tile avatar>
+                <v-list-tile-action>
+                  <v-checkbox v-model="widgets"></v-checkbox>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>Auto-add widgets</v-list-tile-title>
+                  <v-list-tile-sub-title>Automatically add home screen widgets</v-list-tile-sub-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+          </v-card-text>
+
+          <div style="flex: 1 1 auto;"/>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="dialog2" max-width="500px">
+        <v-card>
+          <v-card-title>
+            Dialog 2
+          </v-card-title>
+          <v-card-text>
+            <v-btn color="primary" dark @click.stop="dialog3 = !dialog3">Open Dialog 3</v-btn>
+            <v-select
+              :items="select"
+              label="A Select List"
+              item-value="text"
+            ></v-select>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" flat @click.stop="dialog2=false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="dialog3" max-width="500px">
+        <v-card>
+          <v-card-title>
+            <span>Dialog 3</span>
+            <v-spacer></v-spacer>
+            <v-menu bottom left>
+              <v-btn icon slot="activator">
+                <v-icon>more_vert</v-icon>
+              </v-btn>
+              <v-list>
+                <v-list-tile v-for="(item, i) in items" :key="i" @click="">
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+            </v-menu>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn color="primary" flat @click.stop="dialog3=false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
+      <v-speed-dial
+        v-model="fab"
+        direction="top"
+        transition="slide-y-reverse-transition"
+        style="position: fixed; bottom: 50px; right: 16px;">
+        <v-btn dark fab hover
+          slot="activator"
+          color="blue darken-2"
+          v-model="fab"
+          @click.native="showSpeedDialTooltips = !showSpeedDialTooltips"
+        >
+          <v-icon>account_circle</v-icon>
+          <v-icon>close</v-icon>
+        </v-btn>
+
+        <v-btn fab dark small color="green">
+          <v-icon>edit</v-icon>
+        </v-btn>
+        <v-btn fab dark small color="indigo">
+          <v-icon>add</v-icon>
+        </v-btn>
+        <v-btn fab dark small color="red">
+          <v-icon>file_upload</v-icon>
+        </v-btn>
+      </v-speed-dial>
+
+    </v-content>
 
 
     <v-footer color="indigo" app>
       <span class="white--text">&copy; 2018 Udel Dela Cruz Jr</span>
     </v-footer>
+
   </v-app>
 </template>
 <script>
@@ -97,34 +209,42 @@ export default {
   data () {
     return {
       fab: false,
+      showSpeedDialTooltips: false,
       drawer: null,
-      items: [
+      itemscard: [
         { action: '15 min', headline: 'Brunch this weekend?', title: 'Ali Connors', subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
         { action: '2 hr', headline: 'Summer BBQ', title: 'me, Scrott, Jennifer', subtitle: "Wish I could come, but I'm out of town this weekend." },
-        { action: '6 hr', headline: 'Oui oui', title: 'Sandra Adams', subtitle: 'Do you have Paris recommendations? Have you ever been?' },
-        { action: '12 hr', headline: 'Birthday gift', title: 'Trevor Hansen', subtitle: 'Have any ideas about what we should get Heidi for her birthday?' },
-        { action: '18hr', headline: 'Recipe to try', title: 'Britta Holt', subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.' },
-        { action: '15 min', headline: 'Brunch this weekend?', title: 'Ali Connors', subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
-        { action: '2 hr', headline: 'Summer BBQ', title: 'me, Scrott, Jennifer', subtitle: "Wish I could come, but I'm out of town this weekend." },
-        { action: '6 hr', headline: 'Oui oui', title: 'Sandra Adams', subtitle: 'Do you have Paris recommendations? Have you ever been?' },
-        { action: '12 hr', headline: 'Birthday gift', title: 'Trevor Hansen', subtitle: 'Have any ideas about what we should get Heidi for her birthday?' },
-        { action: '18hr', headline: 'Recipe to try', title: 'Britta Holt', subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.' },
-        { action: '15 min', headline: 'Brunch this weekend?', title: 'Ali Connors', subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
-        { action: '2 hr', headline: 'Summer BBQ', title: 'me, Scrott, Jennifer', subtitle: "Wish I could come, but I'm out of town this weekend." },
-        { action: '6 hr', headline: 'Oui oui', title: 'Sandra Adams', subtitle: 'Do you have Paris recommendations? Have you ever been?' },
-        { action: '12 hr', headline: 'Birthday gift', title: 'Trevor Hansen', subtitle: 'Have any ideas about what we should get Heidi for her birthday?' },
-        { action: '18hr', headline: 'Recipe to try', title: 'Britta Holt', subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.' },
-        { action: '15 min', headline: 'Brunch this weekend?', title: 'Ali Connors', subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
-        { action: '2 hr', headline: 'Summer BBQ', title: 'me, Scrott, Jennifer', subtitle: "Wish I could come, but I'm out of town this weekend." },
-        { action: '6 hr', headline: 'Oui oui', title: 'Sandra Adams', subtitle: 'Do you have Paris recommendations? Have you ever been?' },
-        { action: '12 hr', headline: 'Birthday gift', title: 'Trevor Hansen', subtitle: 'Have any ideas about what we should get Heidi for her birthday?' },
-        { action: '18hr', headline: 'Recipe to try', title: 'Britta Holt', subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.' },
-        { action: '15 min', headline: 'Brunch this weekend?', title: 'Ali Connors', subtitle: "I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
-        { action: '2 hr', headline: 'Summer BBQ', title: 'me, Scrott, Jennifer', subtitle: "Wish I could come, but I'm out of town this weekend." },
-        { action: '6 hr', headline: 'Oui oui', title: 'Sandra Adams', subtitle: 'Do you have Paris recommendations? Have you ever been?' },
-        { action: '12 hr', headline: 'Birthday gift', title: 'Trevor Hansen', subtitle: 'Have any ideas about what we should get Heidi for her birthday?' },
-        { action: '18hr', headline: 'Recipe to try', title: 'Britta Holt', subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.' }
-      ]
+        { action: '6 hr', headline: 'Oui oui', title: 'Sandra Adams', subtitle: 'Do you have Paris recommendations? Have you ever been?' }
+      ],
+      dialog: false,
+        dialog2: false,
+        dialog3: false,
+        notifications: false,
+        sound: true,
+        widgets: false,
+        items: [
+          {
+            title: 'Click Me'
+          },
+          {
+            title: 'Click Me'
+          },
+          {
+            title: 'Click Me'
+          },
+          {
+            title: 'Click Me 2'
+          }
+        ],
+        select: [
+          { text: 'State 1' },
+          { text: 'State 2' },
+          { text: 'State 3' },
+          { text: 'State 4' },
+          { text: 'State 5' },
+          { text: 'State 6' },
+          { text: 'State 7' }
+        ]
     }
   }
 }
